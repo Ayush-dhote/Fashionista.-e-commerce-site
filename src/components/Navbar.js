734349ced86a200045c26a6fb15d1916058c033c
ayush dhote <ayushdhote2001@gmail.com>
2023-08-26 12:29:09 +0530

    updated

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,13 @@
-import React, { useEffect } from 'react'
+import React, { useContext, useEffect } from 'react'
+import { useRef } from 'react'
 import { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
-// import { productContext } from './Context'
+import { productContext } from './Context'
 
 
 const Navbar = () => {
 
-    // const {cartItems}=useContext(productContext)
+    const {cartItems}=useContext(productContext)
     // const quantity=cartItems[id]
     
     const navi=useNavigate()
@@ -32,10 +33,17 @@ const Navbar = () => {
         }
     },[location])
 
+    const leftDivRef = useRef(null);
+    const accessDiv = () => {
+      if (leftDivRef.current) {
+        // Manipulate the div element here
+        leftDivRef.current.style.display = 'none';
+      }
+    };
     
   return (
     <div id='nav'>
-            <div id='left'><p>Fashionista<span>.</span></p></div>
+            <div id='left' ref={leftDivRef}><p>Fashionista<span>.</span></p></div>
             <div id='right'>
                 {isLoggedIn ? <p>🙋‍♂️{name}</p> : <p>🙋‍♂️ <span>Guest</span></p>}
                 <Link to='/cart' id='cart'><span>cart</span> <img id='cp' src="../cart.png" alt="Image" /></Link>
