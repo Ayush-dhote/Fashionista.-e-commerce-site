d6a832d3e01af2e553dc4c1a55ef958add96e0a7
ayush dhote <ayushdhote2001@gmail.com>
2023-08-26 10:13:04 +0530

    updates

--- src/components/Home.js
+++ src/components/Home.js
@@ -14,6 +14,19 @@ const Home = () => {
     <div id='home'>
         <Navbar />
         <div className='titleText'><h2>Newly Arrived</h2></div>
+        <div id="loader">
+        <div id="wrapperload">
+            <div class="elem">
+            <h1>Newly Arrived</h1>
+            </div>
+            <div class="elem">
+                <h1>UI/UX Designer</h1>
+            </div>
+            <div class="elem">
+                <h1>loves to help brands.</h1>
+            </div>
+        </div>
+    </div>
         <div className='products'>
             {Products.map((element)=>{
                return <Product key={element.id} data={element}/>
